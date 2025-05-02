875acd1b6ee78862489eecd50feaed3ea134dd2d
pass0301-admin <yusei2589@icloud.com>
2025-05-03 06:18:28 +0900

    main修復中

--- AGS/Object/Camera.cpp
+++ AGS/Object/Camera.cpp
@@ -1,2 +1,41 @@
 #include "Camera.h"
 #include "DxLib.h"
+#include "Player.h"
+
+void Camera::Init(Player* player)
+{
+	player_ = player;
+
+	// カメラ座標の初期化
+	cameraPos_ = INIT_CAMERA_POS;
+}
+
+void Camera::Update(void)
+{
+	// カメラの前方運動量
+	cameraPos_.z += CAMERA_MOVE_POWER_Z;
+	// カメラX座標をプレイヤーX座標と同期させる
+	cameraPos_.x = player_->GetPlayerPos().x;
+
+	// カメラ設定
+	SetCameraPositionAndAngle(
+		cameraPos_, 0.4f, 0.0f, 0.0f);
+}
+
+void Camera::Draw(void)
+{
+}
+
+void Camera::Release(void)
+{
+}
+
+void Camera::SetCameraPos(VECTOR cameraPos)
+{
+	cameraPos_ = cameraPos;
+}
+
+VECTOR Camera::GetCameraPos(void)
+{
+	return cameraPos_;
+}
--- AGS/Object/Camera.h
+++ AGS/Object/Camera.h
@@ -1,17 +1,19 @@
 #pragma once
+class Player;
+
 class Camera
 {
 
 public:
 	Camera();
 	~Camera();
-	void Init(void);
+	void Init(Player* player);
 	void Update(void);
 	void Draw(void);
 	void Release(void);
 
 private:
-
+	Player* player_;
 	
 };
 
