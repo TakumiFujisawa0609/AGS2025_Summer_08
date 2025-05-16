b8b1caa8a72d614d82ca4a583639d8c8928f804c
ASOST\2416003 <syu10052@gmail.com>
2025-05-16 16:33:22 +0900

    ステージ追加（仮）

--- AGS/Object/Camera.cpp
+++ AGS/Object/Camera.cpp
@@ -7,18 +7,40 @@ void Camera::Init(void)
 {
 	
 
-	center_X = application_->SCREEN_SIZE_X / 2;
-	center_Y = application_->SCREEN_SIZE_Y / 2;
+	//center_X = application_->SCREEN_SIZE_X / 2;
+	//center_Y = application_->SCREEN_SIZE_Y / 2;
 
+	//初期位置
 	nowPos_X = application_->SCREEN_SIZE_X / 2;
 	nowPos_Y = application_->SCREEN_SIZE_Y / 2;
+
+	cameraAngle_ = VGet(0.0f, 0.0f, 0.0f);
+
+	SetMousePoint(center_X, center_Y);
+
+	test = 0;
+	
 	
 }
 
 
 void Camera::Update(void)
 {
-	SetMousePoint(nowPos_X, nowPos_Y);
+
+	MouseLmit();
+	AddAngle();
+
+	
+	if (nowPos_X >= testPos)
+	{
+		test += 1;
+		testPos -= testPos;
+	}
+
+	GetMousePoint(&nowPos_X, &nowPos_Y);
+
+	testPos = nowPos_X + MOVE_CONTROL;
+
 
 
 	// カメラ座標をプレイヤー座標と同期させる
@@ -26,35 +48,25 @@ void Camera::Update(void)
 	if (CheckHitKey(KEY_INPUT_D) == true) cameraPos_.x -= 20;
 
 
-
-	//ここから
-	//マウスの移動制限
-	if (nowPos_X > center_X)
-	{
-		nowPos_X = center_X;
-	}
-
-	if (nowPos_Y > center_Y)
-	{
-		SetMousePoint(center_X, center_Y);
-	}
 	
-	if (center_X > nowPos_X)
-	{
-		SetMousePoint(center_X, center_Y);
-	}
-	if (center_Y > nowPos_Y)
-	{
-		SetMousePoint(center_X, center_Y);
-	}
 }
 
 void Camera::Draw(void)
 {
 
+
+	SetCameraPositionAndAngle(cameraPos_, cameraAngle_.x, cameraAngle_.y, cameraAngle_.z);
+
+	DrawFormatString(0, 50, 0xffffff, "Angle : (%f, %f, %f)", cameraAngle_.x, cameraAngle_.y, cameraAngle_.z);
 	DrawFormatString(0, 0, 0xffffff, "cameraPos : (%f, %f, %f)", cameraPos_.x, cameraPos_.y, cameraPos_.z);
 	// 座標文字列を描く
 	DrawFormatString(0, 100, 0xffffff, "nowPos : %d,%d", nowPos_X, nowPos_Y);
+
+
+
+	DrawFormatString(0, 200, 0xffffff, "test : %d", test);
+
+
 
 }
 
@@ -65,6 +77,55 @@ void Camera::Release(void)
 void Camera::SetCameraPos(VECTOR cameraPos)
 {
 	cameraPos_ = cameraPos;
+
+}
+
+
+
+void Camera::MouseLmit(void)
+{
+
+	//マウスの移動制限
+	if (nowPos_X >= application_->SCREEN_SIZE_X)
+	{
+		nowPos_X = nowPos_X - 10;
+	}
+	if (nowPos_X <= 0)
+	{
+		nowPos_X = nowPos_X + 10;
+	}
+
+	if (nowPos_Y >= application_->SCREEN_SIZE_Y)
+	{
+		nowPos_Y = nowPos_Y - 10;
+	}
+	if (nowPos_Y <= 0)
+	{
+		nowPos_Y = nowPos_Y + 10;
+	}
+}
+
+void Camera::AddAngle(void)
+{
+	//X軸の視点移動
+	if (nowPos_X >= (nowPos_X + MOVE_CONTROL))
+	{
+		cameraAngle_.x =  cameraAngle_.x + ANGLE_CONTROL;
+	}
+	if (nowPos_X <= nowPos_X - MOVE_CONTROL)
+	{
+		cameraAngle_.x -= ANGLE_CONTROL;
+	}
+
+	//Y軸の視点移動
+	if (nowPos_Y >= nowPos_Y + MOVE_CONTROL)
+	{
+		cameraAngle_.y += ANGLE_CONTROL;
+	}
+	if (nowPos_Y <= nowPos_Y - MOVE_CONTROL)
+	{
+		cameraAngle_.y -= ANGLE_CONTROL;
+	}
 }
 
 
--- AGS/Object/Camera.h
+++ AGS/Object/Camera.h
@@ -7,6 +7,9 @@ class Camera
 {
 public:
 
+	static constexpr int MOVE_CONTROL = 6;
+	static constexpr int ANGLE_CONTROL = 1;
+
 	// カメラ初期位置
 	static constexpr VECTOR INIT_CAMERA_POS = { 0.0f, 0.0f, 1000.0f };
 	// カメラ初期角度
@@ -18,6 +21,9 @@ public:
 	void Release(void);
 	void SetCameraPos(VECTOR cameraPos);
 
+	void MouseLmit(void);
+	void AddAngle(void);
+
 
 private:
 	// プレイヤー
@@ -28,8 +34,12 @@ private:
 	// カメラ角度
 	VECTOR cameraAngle_;
 
+	int test_X;
+	int test_Y;
 
+	int test;
 
+	int testPos;
 
 	//マウス中心固定(ゲームシーン用）
 	int center_X;
@@ -37,4 +47,10 @@ private:
 	//マウスの移動先
 	int nowPos_X;
 	int nowPos_Y;
+
+
+
+
+	int addPos_X;
+	int addPos_Y;
 };
