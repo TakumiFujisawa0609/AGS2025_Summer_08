1890a70471ad7f12fe3e17c8534fba53c689bcf4
ASOST\2416003 <syu10052@gmail.com>
2025-05-23 17:17:09 +0900

    カメラ移動視点移動

--- AGS/Object/Camera.cpp
+++ AGS/Object/Camera.cpp
@@ -7,24 +7,23 @@
 void Camera::Init(Player* player)
 {
 	player_ = player;	// ゲームシーン内のプレイヤーポインタを取得
-
 	// カメラ座標の初期化
-	cameraPos_ = INIT_CAMERA_POS;
+	//cameraPos_ = VGet(player_->GetPPos().x, player_->GetPPos().y + 50, player_->GetPPos().z);
 	// カメラ角度初期化
 	cameraAngle_ = INIT_CAMERA_ANGLE;
+	//マウスの初期位置設定
+	//SetMousePoint(center_X, center_Y);
+	//カメラアングル初期設定
+	angle_ = VGet(0.0f, 0.0f, 0.0f);
 
-	angle_X = 0.0f;
-	angle_Y = 0.0f;
-	angle_Z = 0.0f;
+	// カメラ設定
+	SetCameraPositionAndAngle(cameraPos_, angle_.x, angle_.y, angle_.z);
 
-	//初期位置
-	nowPos_X = application_->SCREEN_SIZE_X / 2;
-	nowPos_Y = application_->SCREEN_SIZE_Y / 2;
+	//center_X = application_->SCREEN_SIZE_X / 2;
+	//center_Y = application_->SCREEN_SIZE_Y / 2;
+
 
 	//マウス初期位置
-	SetMousePoint(center_X, center_Y);
-	// カメラ設定
-	SetCameraPositionAndAngle(cameraPos_, angle_X, angle_Y, angle_Z);
 	test = 0;
 	
 }
@@ -32,20 +31,22 @@ void Camera::Init(Player* player)
 
 void Camera::Update(void)
 {
+
 	// カメラ設定
-	SetCameraPositionAndAngle(cameraPos_, angle_X, angle_Y, angle_Z);
+	SetCameraPositionAndAngle(cameraPos_, angle_.x, angle_.y, angle_.z);
 
 	VECTOR playerPos = player_->GetPPos();
-	
+	cameraPos_ = VGet(playerPos.x, playerPos.y + 100, playerPos.z - 20);
+
+
+
+	prevPos_X = nowPos_X;
+	prevPos_Y = nowPos_Y;
 	GetMousePoint(&nowPos_X, &nowPos_Y);
-
-	cameraPos_ = playerPos;
-	cameraPos_.z -= 500.0f;
-	cameraPos_.y += 120.0f;
+	mousePow_X = nowPos_X - prevPos_X;
+	mousePow_Y = nowPos_Y - prevPos_Y;
 	
-	if (CheckHitKey(KEY_INPUT_UP)) cameraPos_.z += 10.0f;
-	if (CheckHitKey(KEY_INPUT_DOWN)) cameraPos_.z -= 10.0f;
-
+	//デバッグ
 	float anglesPowRad = 1.0f * DX_PI_F / 180;
 	if (CheckHitKey(KEY_INPUT_LEFT)) cameraAngle_.y -= anglesPowRad;
 	if (CheckHitKey(KEY_INPUT_RIGHT)) cameraAngle_.y += anglesPowRad;
@@ -54,8 +55,6 @@ void Camera::Update(void)
 	MouseLmit();
 	AddAngle();
 	
-
-
 	if (nowPos_X > test_X)
 	{
 		test += 1;
@@ -66,8 +65,6 @@ void Camera::Update(void)
 		test += 1;
 		test_X = nowPos_X ;
 	}
-	
-
 	if (nowPos_Y > test_Y)
 	{
 		test += 1;
@@ -78,25 +75,23 @@ void Camera::Update(void)
 		test += 1;
 		test_Y = nowPos_Y ;
 	}
-	
 
-	//---------------------------------------↑
-
-	
+	if (angle_.y >= 360.0f)
+	{
+		angle_.y = 0.0f;
+	}
 	
 }
 
 void Camera::Draw(void)
 {
 
-	DrawFormatString(0, 60, 0xffffff, "Angle : (%f, %f, %f)", angle_X, angle_Y, angle_Z);
+	DrawFormatString(0, 60, 0xffffff, "Angle : (%f, %f, %f)", angle_.x, angle_.y, angle_.z);
 	DrawFormatString(0, 0, 0xffffff, "cameraPos : (%f, %f, %f)", cameraPos_.x, cameraPos_.y, cameraPos_.z);
 	// 座標文字列を描く
 	DrawFormatString(0, 100, 0xffffff, "nowPos : %d,%d", nowPos_X, nowPos_Y);
-
-	DrawFormatString(0, 200, 0xffffff, "test : %d", test);
-
-	DrawFormatString(0, 150, 0xffffff, "testPos : %d ,%d", test_X,test_Y);
+	DrawFormatString(0, 200, 0xffffff, "mousePow_X : %d", mousePow_X);
+	DrawFormatString(0, 150, 0xffffff, "cameraPow_X : %d", cameraPow_X);
 
 }
 
@@ -114,10 +109,6 @@ void Camera::SetCameraPos(VECTOR cameraPos)
 
 void Camera::MouseLmit(void)
 {
-	mousePow_X = nowPos_X - center_X;
-	mousePow_Y = nowPos_Y - center_Y;
-
-
 	//マウスの移動制限
 	int limit = 10;
 	//マウスの移動制限
@@ -138,21 +129,54 @@ void Camera::MouseLmit(void)
 	{
 		nowPos_Y = limit;
 	}
-
 	SetMousePoint(nowPos_X, nowPos_Y);
+
+	
 }
 
 void Camera::AddAngle(void)
 {
-	int lowPow = 0;
 
-	if (mousePow_X > lowPow)
+	int lowPow = 1;
+	rad = ANGLE_DEG * DX_PI_F / 180;
+
+	//mousePow_X = nowPos_X - prevPos_X;
+	//mousePow_Y = nowPos_Y - prevPos_Y;
+
+
+	//マウスでplayerの視点移動
+	if (abs(mousePow_X) > lowPow)
 	{
-		cameraPow_X = mousePow_X / MOVE_CONTROL;
-		angle_X = AsoUtility::Deg2RadF(cameraPow_X);
-	}
-	angle_X += AsoUtility::Deg2RadF(mousePow_X);
+		cameraPow_X = mousePow_X / MOUSE_MOVE_CONTROL;
+		angle_.y += AsoUtility::Deg2RadF(cameraPow_X);
 	
+	}
+
+	if (abs(mousePow_Y) > lowPow)
+	{
+		cameraPow_Y = mousePow_Y / MOUSE_MOVE_CONTROL;
+		angle_.x += AsoUtility::Deg2RadF(cameraPow_Y);
+		if (angle_.x > MAX_ANGLE_X_RAD)
+		{
+			angle_.x = MAX_ANGLE_X_RAD;
+		}
+		if (angle_.x < MIN_ANGLE_X_RAD)
+		{
+			angle_.x = MIN_ANGLE_X_RAD;
+		}
+	}
+
+
+
+	//if (mousePow_X < lowPow)
+	//{
+	//	//angle_.y = rad - (mousePow_X / MOUSE_MOVE_CONTROL);
+
+	//
+	//	cameraPow_X = mousePow_X / MOUSE_MOVE_CONTROL;
+	//	angle_.y -= AsoUtility::Deg2RadF(cameraPow_X);
+
+	//}
 }
 
 
